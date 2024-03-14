d23be8f5392968debe3b78816c11d846ec56bd99
vdi-terminal <vdi-terminal@EDU-WSB.local>
2024-03-14 10:44:53 +0100

    zmiana v2

--- hello_world/views.py
+++ hello_world/views.py
@@ -3,7 +3,7 @@ from hello_world.formater import get_formatted
 from hello_world.formater import SUPPORTED, PLAIN
 from flask import request
 
-moje_imie = "Olivier"
+moje_imie = "OlivierSTADNIK"
 msg = "Hello World!"
 
 @app.route('/')
